52940d98d7b951a5784f70990baa7720195a5290
Hemanthkumarkatikireddi <khemanthkumar360@gmail.com>
2023-02-14 21:06:06 +0000

    nxtWatchApp

---
+++ src/components/Banner/StyledComponents.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+// eslint-disable-next-line import/prefer-default-export
+export const BannerDiv = styled.div`
+  background-image: url('https://assets.ccbp.in/frontend/react-js/nxt-watch-banner-bg.png');
+  background-size: cover;
+  height: 25vh;
+  width: 80vw;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`
